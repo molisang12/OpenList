8b9f4b38c0d4d4cadca80864450b20ba2701e85f
molisang12 <lijirou12@outlook.com>
2025-08-30 23:16:15 +0800

    build.sh
    Signed-off-by: molisang12 <lijirou12@outlook.com>

--- build.sh
+++ build.sh
@@ -27,7 +27,8 @@ elif [ "$1" = "beta" ]; then
 else
   git tag -d beta || true
   # Always true if there's no tag
-  version=$(git describe --abbrev=0 --tags 2>/dev/null || echo "v0.0.0")
+  #version=$(git describe --abbrev=0 --tags 2>/dev/null || echo "v0.0.0")
+  VERSION="1.0.0-render-deploy"
   webVersion=$(eval "curl -fsSL --max-time 2 $githubAuthArgs \"https://api.github.com/repos/$frontendRepo/releases/latest\"" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 fi
 
